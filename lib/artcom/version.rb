c4d10cad90c38da8811732c111ab353427b3ede6
Gunnar Marten <gunnar.marten@artcom.de>
2014-02-26 17:59:59 +0100

    removed gemfile lock; added version.rb; added ruby gemset and ruby version files

---
+++ lib/artcom/version.rb
@@ -0,0 +1,12 @@
+class CapistranoASL
+  module Version
+    MAJOR = 0
+    MINOR = 1
+    PATCH = 1
+    BUILD = ''
+
+    STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
+  end
+end
+    end
+end
